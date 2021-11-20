b316d2991e5cb380df1966ca18d03b1da99cd285
philipp <philipp.armingeon@googlemail.com>
2021-11-20 17:03:01 +0100

    Updated code to provide methods of non-local types in custom type

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,8 +1,20 @@
 package mytypes
 
+import (
+	"strings"
+)
+
 type MyInt int
 
 type MyString string
+
+type MyBuilder struct {
+	strings.Builder
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
 
 func (i MyInt) Twice() MyInt {
 	return i * 2
@@ -10,4 +22,12 @@ func (i MyInt) Twice() MyInt {
 
 func (s MyString) Len() int {
 	return len(s)
+}
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+func (su StringUppercaser) ToUpper() string {
+	return strings.ToUpper(su.Contents.String())
 }
--- mytypes/mytypes_test.go
+++ mytypes/mytypes_test.go
@@ -2,6 +2,7 @@ package mytypes_test
 
 import (
 	"mytypes"
+	"strings"
 	"testing"
 )
 
@@ -24,5 +25,53 @@ func TestMyStringLen(t *testing.T) {
 
 	if want != got {
 		t.Errorf("Len: Lengths returned for target string %q do not match. want: %d, got: %d", input, want, got)
+	}
+}
+
+func TestStringsBuilder(t *testing.T) {
+	t.Parallel()
+	var sb strings.Builder
+	sb.WriteString("Hello, ")
+	sb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := sb.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+	wantLen := 15
+	gotLen := sb.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q want len %d, got %d", sb.String(), wantLen, gotLen)
+	}
+}
+
+func TestMyBuilderHello(t *testing.T) {
+	t.Parallel()
+	var mb mytypes.MyBuilder
+	mb.WriteString("Hello, ")
+	mb.WriteString("Gophers!")
+	want := "Hello, Gophers!"
+	got := mb.String()
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
+	}
+
+	wantLen := 15
+	gotLen := mb.Len()
+	if wantLen != gotLen {
+		t.Errorf("%q want len %d, got %d", mb.String(), wantLen, gotLen)
+	}
+}
+
+func TestStringUpperase(t *testing.T) {
+	t.Parallel()
+	var su mytypes.StringUppercaser
+	su.Contents.WriteString("Hello, ")
+	su.Contents.WriteString("Gophers!")
+	want := "HELLO, GOPHERS!"
+	got := su.ToUpper()
+
+	if want != got {
+		t.Errorf("want %q, got %q", want, got)
 	}
 }
